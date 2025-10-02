5b9def3d2b73ca3b18c25db38a722b9b68fbe138
MahmoudKali <itsmahmouddamouni@gmail.com>
2025-10-02 09:26:59 +0000

    Fix: Prevent AsyncStorage error during EAS update

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,17 @@
 // In: lib/supabase.ts
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import { Platform } from 'react-native';
 import 'react-native-url-polyfill/auto';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+const emptyStorage = {
+	getItem: () => null,
+	setItem: () => {},
+	removeItem: () => {},
+};
+
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Supabase URL and/or Anon Key are missing from .env file.");
@@ -12,7 +19,7 @@ if (!supabaseUrl || !supabaseAnonKey) {
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: AsyncStorage, // <-- تم التحديث هنا
+    storage: Platform.OS !== 'web' ?  AsyncStorage : emptyStorage, // <-- تم التحديث هنا
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
